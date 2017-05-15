d343216bac4fb49e7026335fa34637e235c64584
ANTÔNIO COSTA <ANTONIOFC@users.noreply.github.com>
2017-05-15 08:41:59 -0300

    Update config.js

--- server/config.js
+++ server/config.js
@@ -1,12 +1,3 @@
-/*
-
-Arquivo: config.js
-Data: 12/04/2017
-Observação: Arquivo será responsável por armazenar diferentes variáveis e principalmente a configuração da nossa aplicação.
-Author: Glaucia Lemos
-
-*/
-
 module.exports = {
     'configName': 'mDatabase',
     //'database' : 'mongodb://home-user:home123@ds129090.mlab.com:29090/task-list' // via mLab
