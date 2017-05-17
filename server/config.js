238c265cc6d8162fc3d70ebb148d25e721b96fff
antoniofc <antoniofc@gmail.com>
2017-05-16 21:44:07 -0300

    Configurações

--- server/config.js
+++ server/config.js
@@ -9,6 +9,6 @@ Author: Glaucia Lemos
 
 module.exports = {
     'configName': 'mDatabase',
-    //'database' : 'mongodb://home-user:home123@ds129090.mlab.com:29090/task-list' // via mLab
-    'database': 'mongodb://localhost/reddit' // localmente
+    'database' : 'mongodb://home-user:home123@ds129090.mlab.com:29090/task-list' // via mLab
+    //'database': 'mongodb://localhost/reddit' // localmente
 }
