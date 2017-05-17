6e8eafea5c8c02586eff485d5efd07f736aea87b
antoniofc <antoniofc@gmail.com>
2017-05-16 23:31:11 -0300

    V0.2

--- server/index.js
+++ server/index.js
@@ -1,5 +1,7 @@
 import app from './app';
 
-app.listen(process.env.PORT || 3000, () => {
+var port = process.env.PORT || 3000;
+
+app.listen(port, () => {
     console.log('Programa rodando na porta 3000 ...');
 });
