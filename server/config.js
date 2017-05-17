dacdecc5cb2f50b7957b2d467666ba6d844c2c7d
antoniofc <antoniofc@gmail.com>
2017-05-16 21:51:42 -0300

    Config

--- server/config.js
+++ server/config.js
@@ -1,11 +1,5 @@
 module.exports = {
     'configName': 'mDatabase',
-<<<<<<< HEAD
     'database' : 'mongodb://home-user:home123@ds129090.mlab.com:29090/task-list' // via mLab
     //'database': 'mongodb://localhost/reddit' // localmente
 }
-=======
-    //'database' : 'mongodb://home-user:home123@ds129090.mlab.com:29090/task-list' // via mLab
-    'database': 'mongodb://localhost/reddit' // localmente
-}
->>>>>>> d343216bac4fb49e7026335fa34637e235c64584
